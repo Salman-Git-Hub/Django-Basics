2188b42a3576176a895da93b20748528220235a6
Salman Ali <sa1713791@gmail.com>
2022-08-05 21:57:46 +0545

    Model forms

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -14,3 +15,15 @@ def product_detail_view(request):
         "object": obj
     }
     return render(request, 'products/product_detail.html', ctx)
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    ctx = {
+        "form": form
+    }
+    return render(request, 'products/product_create.html', ctx)
