9e48a1c0ca3ccb2f6e4b8d85d0fc1cdc6784460a
Salman Ali <sa1713791@gmail.com>
2022-08-05 21:30:18 +0545

    render data from db

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    # ctx = {
+    #     "title": obj.title,
+    #     "price": obj.price,
+    #     "description": obj.description
+    # }
+    ctx = {
+        "object": obj
+    }
+    return render(request, 'product/detail.html', ctx)
