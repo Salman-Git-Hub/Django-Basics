54dd994116f2035cc4a16c77bcb4e927005925d6
Salman Ali <sa1713791@gmail.com>
2022-09-18 21:40:25 +0545

    urls reverse function

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('home/', home_view, name='home'),
     path('contact/', contact_view),
-    path("products/<int:id>/", dynamic_lookup_view, name='product'),
+    path("products/<int:id>/", dynamic_lookup_view, name='product-view'),
     path("products/<int:id>/delete/", product_delete_view, name='product-delete'),
     path("products/", product_list_view, name='product-list'),
     path('admin/', admin.site.urls),
