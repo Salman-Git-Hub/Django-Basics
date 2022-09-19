510d6293eb8f86bf70da02ae4ff6a14b111b22d7
Salman Ali <sa1713791@gmail.com>
2022-09-19 21:16:07 +0545

    Class Based detail view

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     ArticleListView,
+    ArticleDetailView,
 )
 
 
 app_name = 'articles'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
+    path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),  # default name is 'pk'
 ]
 
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -17,4 +17,14 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all()
+
+
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    # Optional
+    # queryset = Article.objects.all()
+
+    def get_object(self, queryset=None):
+        id_ = self.kwargs.get("id")  # get the url parameter
+        return get_object_or_404(Article, id=id_)
 
